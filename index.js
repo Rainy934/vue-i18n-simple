005bbe83568b9488f3aeb75e66c8bf9926aa510e
huanyu.xu <huanyu.xu@reallytek.com>
2017-12-14 18:02:52 +0800

    update

--- index.js
+++ index.js
@@ -1,69 +1,25 @@
 function I18N() {
     var lang = 'en';
-    var $tts = {
-        APP_NAME: {
-            zh: "系统控制",
-            en: "OS Control"
-        },
-        APP_UPGRADE: {
-            zh: "应用更新",
-            en: "App Upgrade"
-        },
-        UPGRADE_PLAN: {
-            zh: "升级计划",
-            en: "Upgrade Plan"
-        },
-        APP_VERSION: {
-            zh: "应用版本",
-            en: "App Version"
-        },
-        APP_MANAGE: {
-            zh: "应用管理",
-            en: "App Manage"
-        },
-        APP_ANALYSIS: {
-            zh: "应用分析",
-            en: "App Analysis"
-        },
-        PLAN_NAME: {
-            zh: "计划名称",
-            en: "Plan Name"
-        },
-        PLAN_STATE: {
-            zh: "计划状态",
-            en: "Plan State"
-        },
-        COUNTRY: {
-            zh: "国家",
-            en: "Country"
-        },
-        MODEL: {
-            zh: "机型",
-            en: "model"
-        },
-        SEARCH: {
-            zh: "查询",
-            en: "Search"
-        },
-        NEW: {
-            zh: "新建",
-            en: "Add"
-        }
-    };
-    function $toggle($root){
+    var $tts = {}
+    function $toggle(langStr, $root){
         function forceUpdate(obj) {
             obj.$forceUpdate()
             obj.$children.forEach(function(item) {
                 forceUpdate(item)
             })
         }
-        lang = (lang == 'en')?'zh':'en'
+        lang = langStr
         forceUpdate($root)
     }
     function $translate(label){
         return $tts[label][lang]
     }
+
+    function $setLanguage(langObj) {
+        $tts = langObj
+    }
     return {
+        $setLanguage: $setLanguage
         $toggle: $toggle,
         $translate: $translate
     }
